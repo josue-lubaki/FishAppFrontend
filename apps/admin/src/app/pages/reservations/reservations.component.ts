471175a9dab3977cbc960841806e2271405fbfd6
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-07-11 02:03:10 -0400

    Creation components Admin App
    - Pages/Categories
    - Pages/Dashboard
    - Pages/orders
    - Pages/products
    - Pages/reservations
    - Pages/users
    - Shared/shell
    - Shared/sidebar

---
+++ apps/admin/src/app/pages/reservations/reservations.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'admin-reservations',
+    templateUrl: './reservations.component.html',
+    styleUrls: ['./reservations.component.sass']
+})
+export class ReservationsComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
